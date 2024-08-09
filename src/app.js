ca16722be7a62fb3ea4183b98c4fd13cd181b637
Segasonics <114384404+Segasonics@users.noreply.github.com>
2024-08-10 01:54:15 +0530

    agian32

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
     credentials: true // Allow cookies and authorization headers
 }))
 
-app.options(process.env.CORS_ORIGIN, cors());
+app.options("*", cors());
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.static("public"));
