599192749c9a42b5a1b9ef0f1af69f9908398e42
Segasonics <114384404+Segasonics@users.noreply.github.com>
2024-08-10 00:53:48 +0530

    finally

--- src/app.js
+++ src/app.js
@@ -4,25 +4,13 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const allowedOrigins = [
-    'https://harmonious-haupia-5d56d3.netlify.app', // Your main Netlify URL
-    'https://66b6667b6801ee5e1c44e25e--harmonious-haupia-5d56d3.netlify.app' // Specific deploy URL
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
+app.use(cors({
+    origin: 'https://notpet.netlify.app', // Replace with the origin of your frontend app
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify the headers you want to allow
+    credentials: true // Allow cookies and authorization headers
+}))
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}))
