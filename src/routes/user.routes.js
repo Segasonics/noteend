65e9a1732ec24f337e7809c8f7171c010be250e7
Segasonics <114384404+Segasonics@users.noreply.github.com>
2024-08-10 17:05:41 +0530

    added jwt mid in routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 const router = Router()
 
 router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+router.route("/login").post(verifyJWT,loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT,logOutUser)
